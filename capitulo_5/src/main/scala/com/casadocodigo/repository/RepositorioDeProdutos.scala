969a965870a6a75343fac1cab1253ba2483498ab
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-08-11 22:20:12 -0300

    desenvolvendo o capitulo 5

--- capitulo_5/src/main/scala/com/casadocodigo/repository/RepositorioDeProdutos.scala
+++ capitulo_5/src/main/scala/com/casadocodigo/repository/RepositorioDeProdutos.scala
@@ -1,5 +1,20 @@
 package com.casadocodigo.repository
 
+import slick.lifted.Tag
+import slick.jdbc.PostgresProfile.api._
+
 class RepositorioDeProdutos extends DBConnection {
+
+  class Produto(tag: Tag) extends Table[(Long, String, Double, Double)](tag, "produto") {
+    def id = column[Long]("id")
+
+    def descricao = column[String]("descricao")
+
+    def preco = column[Double]("preco")
+
+    def quantidade = column[Double]("quantidade")
+
+    def * = (id, descricao, preco, quantidade)
+  }
 
 }
