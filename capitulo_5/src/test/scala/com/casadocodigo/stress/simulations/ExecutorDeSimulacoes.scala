512249bd545cf8a37843e7c698a4d45c73bcb3a2
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-08-22 07:59:41 -0300

    modificando cenarios do Gatling

--- capitulo_5/src/test/scala/com/casadocodigo/stress/simulations/ExecutorDeSimulacoes.scala
+++ capitulo_5/src/test/scala/com/casadocodigo/stress/simulations/ExecutorDeSimulacoes.scala
@@ -9,7 +9,6 @@ class ExecutorDeSimulacoes extends Simulation with SimulacaoDeProdutos with Simu
   setUp(
     cenarioProduto.inject(atOnceUsers(1)),
     cenarioCliente.inject(atOnceUsers(1)),
-    cenarioCliente.inject(rampUsers(200) during (2 minutes)),
     cenarioPedido.inject(constantUsersPerSec(50) during (5 minutes))
   ).protocols(protocolo)
 
