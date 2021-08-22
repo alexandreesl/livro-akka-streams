d15fd46fce48d43f5acf6cc1da381fb53b9e6b64
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-08-22 07:11:53 -0300

    corrigindo endereco do servico de estoque

--- capitulo_5/src/test/scala/com/casadocodigo/stress/simulations/ExecutorDeSimulacoes.scala
+++ capitulo_5/src/test/scala/com/casadocodigo/stress/simulations/ExecutorDeSimulacoes.scala
@@ -1,13 +1,15 @@
 package com.casadocodigo.stress.simulations
 
 import io.gatling.core.Predef._
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class ExecutorDeSimulacoes extends Simulation with SimulacaoDeProdutos with SimulacaoDePedidos with SimulacaoDeClientes with Protocolo {
 
   setUp(
     cenarioProduto.inject(atOnceUsers(1)),
-    cenarioCliente.inject(atOnceUsers(1)),
-    cenarioPedido.inject(atOnceUsers(1))
+    cenarioCliente.inject(constantUsersPerSec(50) during (5 minutes)),
+    cenarioPedido.inject(rampUsers(200) during (2 minutes))
   ).protocols(protocolo)
 
 }
