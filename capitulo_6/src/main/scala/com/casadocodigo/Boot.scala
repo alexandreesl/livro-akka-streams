b5d6cfe7d2476df22d095c48549e76ffefd6f808
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-08-25 19:45:12 -0300

    desenvolvendo o capitulo 6

--- capitulo_6/src/main/scala/com/casadocodigo/Boot.scala
+++ capitulo_6/src/main/scala/com/casadocodigo/Boot.scala
@@ -4,17 +4,12 @@ package com.casadocodigo
 import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.Source
-import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
 
 object Boot extends App {
 
-  implicit val config: Config = ConfigFactory.load(Option(
-    System.getenv("ENVIRONMENT"))
-    .getOrElse(Option(System.getProperty("ENVIRONMENT"))
-      .getOrElse("application")))
   implicit val system: ActorSystem = ActorSystem("AkkaStreams")
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
