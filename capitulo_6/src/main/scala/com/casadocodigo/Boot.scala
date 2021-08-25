610007bf462a0bb09d815cc844b66754211c9059
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-08-25 19:47:17 -0300

    desenvolvendo o capitulo 6

--- capitulo_6/src/main/scala/com/casadocodigo/Boot.scala
+++ capitulo_6/src/main/scala/com/casadocodigo/Boot.scala
@@ -13,10 +13,14 @@ object Boot extends App {
   implicit val system: ActorSystem = ActorSystem("AkkaStreams")
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
-  val source: Source[Int, NotUsed] = Source(1 to 100)
-  val done: Future[Done] = source.runForeach(i => println(i))
+  primeiraStream
 
-  done.onComplete(_ => system.terminate())
+  private def primeiraStream = {
+    val source: Source[Int, NotUsed] = Source(1 to 100)
+    val done: Future[Done] = source.runForeach(i => println(i))
+
+    done.onComplete(_ => println("terminando a execução!"))
+  }
 
 
 }
