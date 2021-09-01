cfea7eecd60574e437901e34bea252a3a391a24a
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-09-01 19:44:12 -0300

    adicionando testes

---
+++ capitulo_7/akka/src/test/scala/com/casadocodigo/actors/synchronous/CallerActorSpec.scala
@@ -0,0 +1,36 @@
+package com.casadocodigo.actors.synchronous
+
+import akka.actor.testkit.typed.CapturedLogEvent
+import akka.actor.testkit.typed.scaladsl.BehaviorTestKit
+import com.casadocodigo.actors.CallerActor.{MensagemChamadora, MensagemSolicitarFinalizacao, MensagemSolicitarProcessamento}
+import com.casadocodigo.actors.CallerActor
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import org.slf4j.event.Level
+
+class CallerActorSpec extends AnyFlatSpec with should.Matchers {
+
+  "O ator paravel" should "processar mensagens de processamento" in {
+
+    val testador: BehaviorTestKit[MensagemChamadora] = BehaviorTestKit[MensagemChamadora](CallerActor.apply())
+    val dado = "Testando!"
+
+    testador.run(MensagemSolicitarProcessamento(dado))
+    testador.logEntries() shouldBe Seq(CapturedLogEvent(Level.INFO, s"solicitando o processamento da mensagem $dado!"))
+
+    testador.isAlive should be(true)
+
+  }
+
+  "O ator paravel" should "processar mensagens de finalizacao e encerrar" in {
+
+    val testador: BehaviorTestKit[MensagemChamadora] = BehaviorTestKit[MensagemChamadora](CallerActor.apply())
+
+    testador.run(MensagemSolicitarFinalizacao())
+
+    testador.logEntries() shouldBe Seq(CapturedLogEvent(Level.INFO, s"solicitando a finalização do processamento!"))
+    testador.isAlive should be(true)
+
+  }
+
+}
--- capitulo_7/akka/src/test/scala/com/casadocodigo/actors/synchronous/StoppableActorSpec.scala
+++ capitulo_7/akka/src/test/scala/com/casadocodigo/actors/synchronous/StoppableActorSpec.scala
@@ -1,31 +1,36 @@
 package com.casadocodigo.actors.synchronous
 
+import akka.actor.testkit.typed.CapturedLogEvent
 import akka.actor.testkit.typed.scaladsl.BehaviorTestKit
 import com.casadocodigo.actors.StoppableActor
 import com.casadocodigo.actors.StoppableActor.{ExcecaoDeFinalizacao, Mensagem, MensagemFinalizar, MensagemProcessamento}
 import org.scalatest._
 import flatspec._
 import matchers._
+import org.slf4j.event.Level
 
 
 class StoppableActorSpec extends AnyFlatSpec with should.Matchers {
 
   "O ator paravel" should "processar mensagens de processamento" in {
 
-    val testKit: BehaviorTestKit[Mensagem] = BehaviorTestKit[Mensagem](StoppableActor.behavior())
+    val testador: BehaviorTestKit[Mensagem] = BehaviorTestKit[Mensagem](StoppableActor.behavior())
 
-    testKit.run(MensagemProcessamento("Testando!"))
+    val dado = "Testando!"
 
-    testKit.isAlive should be(true)
+    testador.run(MensagemProcessamento(dado))
+
+    testador.logEntries() shouldBe Seq(CapturedLogEvent(Level.INFO, s"processando a mensagem $dado!"))
+    testador.isAlive should be(true)
 
   }
 
   "O ator paravel" should "processar mensagens de finalizacao e encerrar" in {
 
-    val testKit: BehaviorTestKit[Mensagem] = BehaviorTestKit[Mensagem](StoppableActor.behavior())
+    val testador: BehaviorTestKit[Mensagem] = BehaviorTestKit[Mensagem](StoppableActor.behavior())
 
     assertThrows[ExcecaoDeFinalizacao] {
-      testKit.run(MensagemFinalizar())
+      testador.run(MensagemFinalizar())
     }
 
   }
